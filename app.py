d1c1e88ab98033105af90060c8d05361915c4770
Sai Teja Macharla <18657211+saitej123@users.noreply.github.com>
2021-06-26 19:57:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def main():
                 boxes = [line[0] for line in result]
                 txts = [line[1][0] for line in result]
                 scores = [line[1][1] for line in result]
-                im_show = draw_ocr(image, boxes, txts, scores, font_path='./doc/simfang.ttf')
+                im_show = draw_ocr(image, boxes, txts, scores, font_path='/home/appuser/share/fonts/truetype/freefont/FreeMono.ttf')
                 im_show = Image.fromarray(im_show)
                 st.subheader('Extracted text is ...')
                     
